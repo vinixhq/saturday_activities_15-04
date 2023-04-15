6073621c4c24c2ba2573278af4a3d3fc26608d04
viniciuss <109877390+vinixhq@users.noreply.github.com>
2023-04-15 12:34:30 -0300

    Add files via upload

---
+++ Atividade Arrays 15-04/script.js
@@ -0,0 +1,23 @@
+// Escreva um script que remova o ultimo elemento de um array:
+
+const person = ["Felipe", 30, "São Paulo", "Masculino", "584651671658"];
+
+person.pop();
+
+console.log(person);
+
+// Escreva um script que adiciona um novo elemento no inicio do array:
+
+const jobs = ["Frontend", "Backend", "Devops", "FullStack"];
+
+jobs.unshift("Design");
+
+console.log(jobs);
+
+// Escreva um script que adiciona um elemento no final do array:
+
+const users = ["Ana", "Fernando", "Rafael", "Clara", "Maria"];
+
+users.push("Roger");
+
+console.log(users);
